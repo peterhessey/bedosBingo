b58738df1ef70c42660da3eff1f2d3168f4e2237
Peter <peter.hessey@hotmail.com>
2019-11-08 10:41:43 +0000

    Added pause and reset functionality

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ Ensure your machine has python 3.x and pip installed (using the commands '`pytho
 2. Go into your command prompt and navigate to the directory you downloaded the files to
 3. Run the command: `pip install -r requirements.txt`
 4. Run the command `python bingo.py -h` to see options for running the program (remove the `-h` to run the program using the defaults)
-5. Use the escape key to close the program at any point
+
+### Controls
+-  Use the escape key to close the program at any point
+-  Press P to pause the program at any point (note that the timer does not stop when paused, i.e. when you unpause, if > 15 seconds have passed the next number will display immediately!)
--- bingo.py
+++ bingo.py
@@ -7,7 +7,7 @@ import argparse
 
 BINGO_DATA_FILEPATH = './bingo_data.txt'
 
-def bingoTime(bingo_data, wait_time):
+def bingoTime(bingo_data_in, wait_time):
     """Main function, runs the bingo program using pygame.
     
     Arguments:
@@ -20,6 +20,8 @@ def bingoTime(bingo_data, wait_time):
     #pygame set up
     pygame.init()
 
+    bingo_data = bingo_data_in.copy()
+
     bingo_window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     window_width = bingo_window.get_width()
     window_height = bingo_window.get_height()
@@ -30,56 +32,85 @@ def bingoTime(bingo_data, wait_time):
     new_number = True
     start_time = time.time()
     game_quit = False
+    pause = False
 
     #loops while there is still data in the dictionary
-    while bool(bingo_data) and not game_quit:
+    while not game_quit:
 
         #check if user has tried to quit
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     game_quit = True
+
+                if event.key == pygame.K_p:
+                    pause = not pause
+
+                if event.key == pygame.K_r:
+                    if not bool(bingo_data):
+                        bingo_data = bingo_data_in.copy()
+                        pause = False
+                        new_number = True
+
         
         #select next number if enough time has passed
         if new_number:
-            #selecting random song
-            numbers_left = list(bingo_data.keys())
-            index_selected = random.randint(0, len(numbers_left) - 1)
-            number_to_display = numbers_left[index_selected]
-            text_to_display, song_to_play = bingo_data[number_to_display]
-            bingo_data.pop(number_to_display)
 
-            #prepare text surfaces
-            bingo_window.fill((140, 26, 255))            
-            number_text = number_font.render(str(number_to_display), True,
+            if bool(bingo_data):
+                #selecting random song
+                numbers_left = list(bingo_data.keys())
+                index_selected = random.randint(0, len(numbers_left) - 1)
+                number_to_display = numbers_left[index_selected]
+                text_to_display, song_to_play = bingo_data[number_to_display]
+                bingo_data.pop(number_to_display)
+
+                #prepare text surfaces
+                bingo_window.fill((140, 26, 255))            
+                number_text = number_font.render(str(number_to_display), True,
+                                                (255,255,255))
+                phrase_text = text_font.render(text_to_display, True,
                                             (255,255,255))
-            phrase_text = text_font.render(text_to_display, True,
-                                          (255,255,255))
 
-            #text position on the game window
-            number_pos = (window_width /2 - number_text.get_width() / 2, 
-                          window_height/2*0.7 - number_text.get_height() / 2)
+                #text position on the game window
+                number_pos = (window_width /2 - number_text.get_width() / 2, 
+                            window_height/2*0.7 - number_text.get_height() / 2)
 
-            phrase_pos = (window_width /2 - phrase_text.get_width() / 2, 
-                          window_height/2*1.3 - phrase_text.get_height() / 2)
+                phrase_pos = (window_width /2 - phrase_text.get_width() / 2, 
+                            window_height/2*1.3 - phrase_text.get_height() / 2)
 
-            #draw and display the text
-            bingo_window.blit(number_text, (number_pos))
-            bingo_window.blit(phrase_text, (phrase_pos))
+                #draw and display the text
+                bingo_window.blit(number_text, (number_pos))
+                bingo_window.blit(phrase_text, (phrase_pos))
+
+                #reset variables
+                new_number = False
+                start_time = time.time()
+
+            else:
+                bingo_window.fill((140, 26, 255))  
+                end_game_string = "Thanks for playing Bedo's Bingo!"
+                end_game_text = text_font.render(str(end_game_string), True,
+                                                   (255,255,255))
+
+                end_game_pos = (window_width / 2 - end_game_text.get_width() / 2,
+                                window_height/2 - end_game_text.get_height() /2)
+                
+                bingo_window.blit(end_game_text, (end_game_pos))
+            
             pygame.display.update()
 
-            #reset variables
-            new_number = False
-            start_time = time.time()
+            
         
-        #monitor passing of time
-        time_now = time.time()
-        if (time_now - start_time) > wait_time:
-            new_number = True
+        if not pause:
+            #monitor passing of time
+            time_now = time.time()
+            if (time_now - start_time) > wait_time:
+                new_number = True
 
 
 def playSong(song):
     return 0
+
 
 def readBingoFiles():
     """Reads the data from bingo_data.txt
